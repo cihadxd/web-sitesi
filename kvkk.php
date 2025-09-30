38d3f09c799f7d3c6dd7c3ea6bc69bfa019b4224
Cihad <251305028@ogrenci.karatekin.edu.tr>
2025-09-30 21:23:11 +0300

    KVKK ve Gizlilik metni eklendi.

---
+++ kvkk.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>KVKK ve Gizlilik Metni</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-5">
+    <h2>KVKK ve Gizlilik Metni</h2>
+    <p>
+        Kişisel verileriniz, sipariş işlemleri ve yasal yükümlülükler kapsamında saklanmaktadır. Siparişinizin işlenmesi, teslimi ve yasal zorunluluklar için bilgileriniz kullanılabilir.
+        <br><br>
+        Kişisel verileriniz kesinlikle üçüncü şahıslarla paylaşılmaz. KVKK kapsamında dilediğiniz zaman bilgilerinizin silinmesini talep edebilirsiniz.
+        <br><br>
+        Detaylı bilgi ve talepleriniz için <a href="mailto:info@satis-site.com">info@satis-site.com</a> adresinden iletişime geçebilirsiniz.
+    </p>
+    <a href="index.php" class="btn btn-primary mt-3">Anasayfa</a>
+</div>
+</body>
+</html>
